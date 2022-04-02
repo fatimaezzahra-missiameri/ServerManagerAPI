48fb1d879d969befb2c4af53cfd897d368a2a917
Fatima Ezzahra Missiameri <fatimaezzahra.missiameri@gmail.com>
2022-04-02 15:05:30 +0000

    Fix a typo error.

--- src/main/java/com/femissiameri/api/repository/ServerRepository.java
+++ src/main/java/com/femissiameri/api/repository/ServerRepository.java
@@ -4,5 +4,5 @@ import com.femissiameri.api.model.Server;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ServerRepository extends JpaRepository<Server, Long> {
-    Server findByIpAddress(String idAddress);
+    Server findByIpAddress(String ipAddress);
 }
