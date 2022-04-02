f99e45fa7dd7981052468fc15c8e7eb30e69f4c5
Fatima Ezzahra Missiameri <fatimaezzahra.missiameri@gmail.com>
2022-04-02 12:06:18 +0000

    Initial commit.

---
+++ src/test/java/com/femissiameri/api/ServerManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femissiameri.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
